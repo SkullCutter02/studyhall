75323e8f0b82fb2e7edad3a1f296982b17008b0a
skullcutter02 <=>
2022-03-14 11:24:56 +0800

    Configured user interface

--- client/types/user.interface.ts
+++ client/types/user.interface.ts
@@ -1,3 +1,8 @@
-export default interface User {
-  id: string;
+import Base from "./base.interface";
+import Role from "./role.enum";
+
+export default interface User extends Base {
+  name: string;
+  email: string;
+  roles: Role[];
 }
